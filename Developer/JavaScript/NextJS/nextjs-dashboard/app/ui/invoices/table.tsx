fd8d26a003b0c9b2bf7c2caf0f90eabc1e4f5004
MagicBeamish <magicbeamish@protonmail.com>
2023-11-15 16:59:42 -0700

    adding alt property to images in /app/ui/invoices/table.tsx ...again...and again.

--- Developer/JavaScript/NextJS/nextjs-dashboard/app/ui/invoices/table.tsx
+++ Developer/JavaScript/NextJS/nextjs-dashboard/app/ui/invoices/table.tsx
@@ -28,10 +28,10 @@ export default async function InvoicesTable({
                     <div className="mb-2 flex items-center">
                       <Image
                         src={invoice.image_url}
+                        alt="Graph of the most recent invoices"
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
-                        alt=''
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -87,10 +87,10 @@ export default async function InvoicesTable({
                     <div className="flex items-center gap-3">
                       <Image
                         src={invoice.image_url}
+                        alt="Graph of the most recent invoices"
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt=''
                       />
                       <p>{invoice.name}</p>
                     </div>
